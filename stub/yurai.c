e4d3a9af7cb6f5913983d2ff1bd8fd64ddc0054a
MCMrARM <mcmrarm@gmail.com>
2018-12-25 14:53:38 +0100

    Initial commit

---
+++ stub/yurai.c
@@ -0,0 +1,8 @@
+#include <yurai.h>
+
+void yurai_add_library(void *lib) {}
+void yurai_remove_library(void *lib) {}
+void *yurai_dlsym(void *lib, const char *name) {}
+yurai_hook_t yurai_hook(void *lib, const char *name, void *hook, void **orig) {}
+void yurai_delete_hook(yurai_hook_t hook) {}
+void yurai_apply_hooks() {}
